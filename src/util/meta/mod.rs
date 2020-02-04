a132c06df67a694f7da46697eb41838e11a68f84
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-04 14:15:20 +0000

    Added rows macro.

--- src/util/meta/mod.rs
+++ src/util/meta/mod.rs
@@ -4,5 +4,6 @@ pub mod access;
 pub mod args;
 pub mod map;
 pub mod report;
+pub mod rows;
 
-pub use self::{access::*, args::*, map::*, report::*};
+pub use self::{access::*, args::*, map::*, report::*, rows::*};
