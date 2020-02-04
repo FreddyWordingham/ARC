f87680249724867f62f1b85558f4ee50acd82cd9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-04 11:44:13 +0000

    Changed directory name to work with git pages.

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -1,7 +1,7 @@
 cd $ARC_DIR;
 
 cargo doc;
-cp -r $ARC_DIR/target/doc $ARC_DIR/doc;
-echo "<meta http-equiv=refresh content=0;url=arc/index.html>" > $ARC_DIR/doc/index.html;
+cp -r $ARC_DIR/target/doc $ARC_DIR/docs;
+echo "<meta http-equiv=refresh content=0;url=arc/index.html>" > $ARC_DIR/docs/index.html;
 
 cd -;
