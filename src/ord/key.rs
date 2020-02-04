624f8649d2cfd01cc3963f5d3e1c27ae26bf1bd0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-04 17:20:17 +0000

    Added simplified set and key structures.

--- src/ord/key.rs
+++ src/ord/key.rs
@@ -1,40 +1,38 @@
 //! Key alias.
 
-use crate::file::{Load, Save};
-use json5::to_string;
-use std::{
-    fs::{read_to_string, write},
-    path::Path,
-};
+use attr::json;
+use std::fmt::{Display, Formatter, Result};
 
-use serde::{Deserialize, Serialize};
-use std::marker::PhantomData;
+/// Construct a new key type.
+macro_rules! key {
+    ($key:ident) => {
+        #[json]
+        #[derive(Clone, PartialOrd, Eq, PartialEq, Ord)]
+        pub struct $key(String);
 
-/// Set key structure.
-#[derive(Clone, PartialOrd, Eq, PartialEq, Ord, Serialize, Deserialize)]
-pub struct Key<T>(String, PhantomData<T>);
+        impl $key {
+            #[inline]
+            #[must_use]
+            pub fn new(string: &str) -> Self {
+                Self {
+                    0: string.to_string(),
+                }
+            }
 
-impl<T> Save for Key<T> {
-    #[inline]
-    fn save(&self, path: &Path) {
-        write(
-            path,
-            to_string(&self.0).expect("Unable to serialise object."),
-        )
-        .expect("Unable to write json file.");
-    }
-}
-
-impl<T> Load for Key<T> {
-    #[inline]
-    #[must_use]
-    fn load(path: &Path) -> Self {
-        let obj: String = json5::from_str(&read_to_string(path).expect("Unable to read file."))
-            .expect("Unable to parse json file.");
-
-        Self {
-            0: obj,
-            1: PhantomData,
+            #[inline]
+            #[must_use]
+            pub fn str(&self) -> &str {
+                &self.0
+            }
         }
-    }
+
+        impl Display for $key {
+            #[inline]
+            fn fmt(&self, fmt: &mut Formatter) -> Result {
+                write!(fmt, "{}", self.0)
+            }
+        }
+    };
 }
+
+key!(Key);
--- src/ord/set/mod.rs
+++ src/ord/set/mod.rs
@@ -1,21 +1,49 @@
 //! Set implementation.
 
-use crate::{access, ord::Key};
-use std::collections::BTreeMap;
+use crate::{access, file::Load, ord::Key};
+use log::info;
+use std::{collections::BTreeMap, path::Path};
 
 /// Set mapping.
 pub struct Set<T> {
     /// Internal map.
-    map: BTreeMap<Key<T>, T>,
+    map: BTreeMap<Key, T>,
 }
 
 impl<T> Set<T> {
-    access!(map, BTreeMap<Key<T>, T>);
+    access!(map, BTreeMap<Key, T>);
 
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new(map: BTreeMap<Key<T>, T>) -> Self {
+    pub fn new(map: BTreeMap<Key, T>) -> Self {
         Self { map }
+    }
+
+    /// Access a value within the map.
+    #[inline]
+    #[must_use]
+    pub fn get(&self, key: &Key) -> &T {
+        self.map
+            .get(key)
+            .expect(&format!("Key {} does not exist within the set.", key))
+    }
+}
+
+impl<T: Load> Set<T> {
+    /// Load a set of files.
+    #[inline]
+    #[must_use]
+    pub fn load(dir: &Path, keys: &[Key], ext: &str) -> Set<T> {
+        let mut map = BTreeMap::new();
+
+        for key in keys {
+            let path = dir.join(format!("{}.{}", key, ext));
+            info!("Loading: {}", path.display());
+
+            map.insert(key.clone(), T::load(&path));
+        }
+
+        Set::new(map)
     }
 }
