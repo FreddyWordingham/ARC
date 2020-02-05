ee3674cdc461d29c5a2383fd501eefb947645e2e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-05 15:38:05 +0000

    Added set aliases.

---
+++ src/ord/set/react_set.rs
@@ -0,0 +1,9 @@
+//! Reaction set.
+
+// use crate::{
+//     chem::Reaction,
+//     ord::{ReactKey, Set},
+// };
+
+// /// Alias for the reaction set.
+// pub type ReactSet = Set<ReactKey, Reaction>;
---
+++ src/ord/set/spec_set.rs
@@ -0,0 +1,9 @@
+//! Species set.
+
+// use crate::{
+//     chem::Species,
+//     ord::{Set, SurfKey},
+// };
+
+// /// Alias for the species set.
+// pub type SpecSet = Set<SpecKey, Species>;
