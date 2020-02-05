2a796c3874f22f093ba862d0b3a5570369d69d93
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-05 12:36:48 +0000

    Added input form to main.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,15 @@
 //! Main function.
 
-use arc::util::{banner, exec};
+use arc::{
+    args,
+    file::Load,
+    report,
+    util::{banner, exec, init},
+};
 use attr::form;
 use colog;
 use log::info;
+use std::path::PathBuf;
 
 #[form]
 struct Parameters {
@@ -14,5 +20,23 @@ fn main() {
     colog::init();
     banner::title(&exec::name());
 
-    info!("Hello world!");
+    banner::section("initialisation");
+    let (in_dir, out_dir, params_path) = initialisation();
+    report!(in_dir.display(), "input directory");
+    report!(out_dir.display(), "output directory");
+    report!(params_path.display(), "parameters path");
+
+    banner::section("Prelude");
+    let params = Parameters::load(&params_path);
+    info!("loaded parameters file");
+}
+
+fn initialisation() -> (PathBuf, PathBuf, PathBuf) {
+    args!(_bin_path: String;
+        params_name: String);
+
+    let (in_dir, out_dir) = init::io_dirs(None, None);
+    let params_path = &in_dir.join(params_name);
+
+    (in_dir, out_dir, params_path.to_path_buf())
 }
