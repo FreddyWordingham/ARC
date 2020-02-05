4e32441d6c1df94b08fe7e6e5aacbe5c3693900a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-05 17:41:40 +0000

    Added parts to universe.

--- src/ord/set/light_set.rs
+++ src/ord/set/light_set.rs
@@ -1,9 +1,21 @@
 //! Light set.
 
 use crate::{
-    ord::{LightKey, Set},
+    ord::{LightKey, Set, SurfKey},
     world::Light,
 };
 
 /// Alias for the light set.
 pub type LightSet = Set<LightKey, Light>;
+
+impl LightSet {
+    /// Get a list of all surface keys used by the light set.
+    #[inline]
+    #[must_use]
+    pub fn surf_keys(&self) -> Vec<SurfKey> {
+        self.map
+            .values()
+            .map(|light| light.surf().clone())
+            .collect()
+    }
+}
--- src/world/uni/verse.rs
+++ src/world/uni/verse.rs
@@ -1,22 +1,52 @@
 //! Material implementation.
 
-use crate::{access, ord::InterSet};
+use crate::{
+    access,
+    ord::{InterSet, LightSet, MatSet, ReactSet, SpecSet, SurfSet},
+};
 
 /// Material physical properties.
 pub struct Verse {
-    // /// Surfaces.
-    // surfs: SurfSet,
     /// Interfaces.
     inters: InterSet,
+    /// Reactions.
+    reacts: ReactSet,
+    /// Lights.
+    lights: LightSet,
+    /// Meshes.
+    mats: MatSet,
+    /// Species.
+    specs: SpecSet,
+    /// Surfaces.
+    surfs: SurfSet,
 }
 
 impl Verse {
     access!(inters, InterSet);
+    access!(reacts, ReactSet);
+    access!(lights, LightSet);
+    access!(mats, MatSet);
+    access!(specs, SpecSet);
+    access!(surfs, SurfSet);
 
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new(inters: InterSet) -> Self {
-        Self { inters }
+    pub fn new(
+        inters: InterSet,
+        reacts: ReactSet,
+        lights: LightSet,
+        mats: MatSet,
+        specs: SpecSet,
+        surfs: SurfSet,
+    ) -> Self {
+        Self {
+            inters,
+            reacts,
+            lights,
+            mats,
+            specs,
+            surfs,
+        }
     }
 }
