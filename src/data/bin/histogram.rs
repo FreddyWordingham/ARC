d79f27889db911cc397ce5e21ee645e44ae18eaa
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-06 14:48:20 +0000

    Renamed math indexer to binner.

--- src/data/bin/histogram.rs
+++ src/data/bin/histogram.rs
@@ -3,21 +3,21 @@
 use crate::{
     access,
     file::Save,
-    math::{Indexer, Range},
+    math::{Binner, Range},
 };
 use ndarray::Array1;
 use std::{fs::File, io::Write, path::Path};
 
 /// Static range, constant bin width, Histogram.
 pub struct Histogram {
-    /// Indexer.
-    indexer: Indexer,
+    /// Binner.
+    binner: Binner,
     /// Count data.
     counts: Array1<f64>,
 }
 
 impl Histogram {
-    access!(indexer, Indexer);
+    access!(binner, Binner);
     access!(counts, Array1<f64>);
 
     /// Construct a new instance
@@ -28,7 +28,7 @@ impl Histogram {
         assert!(bins > 0);
 
         Self {
-            indexer: Indexer::new(Range::new(min, max), bins),
+            binner: Binner::new(Range::new(min, max), bins),
             counts: Array1::zeros(bins as usize),
         }
     }
@@ -36,26 +36,26 @@ impl Histogram {
     /// Increment the bin corresponding to x by unity.
     #[inline]
     pub fn collect(&mut self, x: f64) {
-        assert!(self.indexer.range().contains(x));
+        assert!(self.binner.range().contains(x));
 
-        let index = self.indexer.index(x);
+        let index = self.binner.bin(x);
         *self.counts.get_mut(index).expect("Invalid index.") += 1.0;
     }
 
     /// Increment the bin corresponding to x by a given weight.
     #[inline]
     pub fn collect_weight(&mut self, x: f64, weight: f64) {
-        assert!(self.indexer.range().contains(x));
+        assert!(self.binner.range().contains(x));
         assert!(weight > 0.0);
 
-        let index = self.indexer.index(x);
+        let index = self.binner.bin(x);
         *self.counts.get_mut(index).expect("Invalid index.") += weight;
     }
 
     /// Increment the bin corresponding to x by unity if x is contained within the range.
     #[inline]
     pub fn try_collect(&mut self, x: f64) {
-        if let Some(index) = self.indexer.try_index(x) {
+        if let Some(index) = self.binner.try_bin(x) {
             *self.counts.get_mut(index).expect("Invalid index.") += 1.0;
         }
     }
@@ -63,7 +63,7 @@ impl Histogram {
     /// Increment the bin corresponding to x by unity if x is contained within the range.
     #[inline]
     pub fn try_collect_weight(&mut self, x: f64, weight: f64) {
-        if let Some(index) = self.indexer.try_index(x) {
+        if let Some(index) = self.binner.try_bin(x) {
             *self.counts.get_mut(index).expect("Invalid index.") += weight;
         }
     }
@@ -75,8 +75,8 @@ impl Save for Histogram {
     fn save(&self, path: &Path) {
         let mut file = File::create(path).expect("Unable to create histogram file.");
 
-        let bin_width = self.indexer.bin_width();
-        let min = self.indexer.range().min();
+        let bin_width = self.binner.bin_width();
+        let min = self.binner.range().min();
 
         for (iter, value) in self.counts.iter().enumerate() {
             let x = (iter as f64 + 0.5).mul_add(bin_width, min);
