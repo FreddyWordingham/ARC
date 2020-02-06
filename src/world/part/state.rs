91669cec18550a3c9f41d05dae9a974752ddc5e8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-06 10:44:16 +0000

    Added conc and source getter methods to state structure.

--- src/world/part/state.rs
+++ src/world/part/state.rs
@@ -2,9 +2,10 @@
 
 use crate::{
     access,
-    ord::{Set, SpecKey},
+    ord::{Set, SpecKey, SpecSet},
 };
 use attr::json;
+use ndarray::Array1;
 
 /// State structure.
 #[json]
@@ -24,5 +25,45 @@ impl State {
     #[must_use]
     pub fn new(concs: Set<SpecKey, f64>, sources: Set<SpecKey, f64>) -> Self {
         Self { concs, sources }
+    }
+
+    /// Create a new complete concentration array.
+    #[inline]
+    #[must_use]
+    pub fn new_conc_arr(&self, specs: SpecSet) -> Array1<f64> {
+        let mut concs = Array1::zeros(specs.map().len());
+
+        let mut index = 0;
+        for (key, c) in self.concs.map() {
+            for (i, k) in specs.map().keys().skip(index).enumerate() {
+                *concs.get_mut(i).expect("Missing concentration element.") = *c;
+
+                if k == key {
+                    index = i + 1;
+                }
+            }
+        }
+
+        concs
+    }
+
+    /// Create a new complete source array.
+    #[inline]
+    #[must_use]
+    pub fn new_source_arr(&self, specs: SpecSet) -> Array1<f64> {
+        let mut sources = Array1::zeros(specs.map().len());
+
+        let mut index = 0;
+        for (key, c) in self.sources.map() {
+            for (i, k) in specs.map().keys().skip(index).enumerate() {
+                *sources.get_mut(i).expect("Missing source element.") = *c;
+
+                if k == key {
+                    index = i + 1;
+                }
+            }
+        }
+
+        sources
     }
 }
