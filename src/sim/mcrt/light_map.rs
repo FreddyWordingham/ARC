b153b0eabc76834c0c134665d4b6ac9c580d28a9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-07 17:30:49 +0000

    Added light_map source.

--- src/sim/mcrt/light_map.rs
+++ src/sim/mcrt/light_map.rs
@@ -1,8 +1,19 @@
 //! Light-Map structure.
 
-use crate::sim::Record;
+use crate::{sim::Record, world::Grid};
 use ndarray::Array3;
 use std::ops::AddAssign;
+
+macro_rules! data_dens {
+    ($dens_func: ident, $prop: ident) => {
+        /// Create a density data-cube of the lightmap's records.
+        #[inline]
+        #[must_use]
+        pub fn $dens_func(&self) -> Array3<f64> {
+            self.recs.map(|rec| rec.$prop / self.cell_vol)
+        }
+    };
+}
 
 /// Light-Map structure implementation.
 /// Stores output data from an MCRT simulation.
@@ -18,15 +29,34 @@ impl LightMap {
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new(res: [usize; 3], cell_vol: f64) -> Self {
-        assert!(res.iter().all(|x| *x > 0));
-        assert!(cell_vol > 0.0);
+    pub fn new(grid: &Grid) -> Self {
+        let res = grid.cells().shape();
+        let cell_vol = grid.bound().vol() / grid.cells().len() as f64;
 
         Self {
-            recs: Array3::default(res),
+            recs: Array3::default([res[0], res[1], res[2]]),
             cell_vol,
         }
     }
+
+    /// Create a list of density mappings.
+    #[inline]
+    #[must_use]
+    pub fn dens_maps(&self) -> Vec<(&str, Array3<f64>)> {
+        vec![
+            ("emission dens", self.emission_dens()),
+            ("scatter dens", self.scatter_dens()),
+            ("absorption dens", self.absorption_dens()),
+            ("shift dens", self.shift_dens()),
+            ("dist travelled dens", self.dist_travelled_dens()),
+        ]
+    }
+
+    data_dens!(emission_dens, emissions);
+    data_dens!(scatter_dens, scatters);
+    data_dens!(absorption_dens, absorptions);
+    data_dens!(shift_dens, shifts);
+    data_dens!(dist_travelled_dens, dist_travelled);
 }
 
 impl AddAssign<&Self> for LightMap {
