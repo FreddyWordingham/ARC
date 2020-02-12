574fa497dd252600bf2be1993743bcd92dc0cebe
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-12 23:30:05 +0000

    Added camera direction.

--- src/sim/imager/camera.rs
+++ src/sim/imager/camera.rs
@@ -1,33 +1,54 @@
 //! Camera structure.
 
-use crate::{access, clone};
+use crate::{access, clone, geom::Ray};
 use attr::json;
-use nalgebra::Point3;
+use nalgebra::{Point3, Unit, Vector3};
 
 /// Image forming structure.
 #[json]
 pub struct Camera {
     /// Image resolution.
-    res: (usize, usize),
+    res: (u64, u64),
     /// Position.
     pos: Point3<f64>,
+    /// Direction.
+    dir: Unit<Vector3<f64>>,
     /// Field of view [deg].
     fov: f64,
 }
 
 impl Camera {
-    clone!(res, (usize, usize));
+    clone!(res, (u64, u64));
     access!(pos, Point3<f64>);
     access!(fov, f64);
 
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new(res: (usize, usize), pos: Point3<f64>, fov: f64) -> Self {
+    pub fn new(res: (u64, u64), pos: Point3<f64>, fov: f64) -> Self {
         debug_assert!(res.0 > 0);
         debug_assert!(res.1 > 0);
         debug_assert!(fov > 0.0);
 
-        Self { res, pos, fov }
+        let dir = Unit::new_normalize(Point3::origin() - pos);
+
+        Self { res, pos, dir, fov }
+    }
+
+    /// Calculate the total number of pixels.
+    #[inline]
+    #[must_use]
+    pub fn num_pix(&self) -> u64 {
+        self.res.0 * self.res.1
+    }
+
+    /// Generate the nth ray.
+    #[inline]
+    #[must_use]
+    pub fn gen_ray(&self, n: u64) -> Ray {
+        debug_assert!(n < self.num_pix());
+
+        let xi = n % self.res.1;
+        let yi = n / self.res.1;
     }
 }
