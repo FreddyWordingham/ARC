67e24a69297caae9bee52b82b26ca2a3a05176c1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-13 14:01:42 +0000

    Simplified camera input.

--- src/file/json/camera.rs
+++ src/file/json/camera.rs
@@ -1,8 +1,8 @@
 //! Camera implementation.
 
-use crate::{access, clone, sim::Camera as SimCamera};
+use crate::{access, clone};
 use attr::json;
-use nalgebra::{Point3, Unit, Vector3};
+use nalgebra::{Point3, Vector3};
 
 /// Camera building structure.
 #[json]
@@ -19,15 +19,4 @@ impl Camera {
     access!(pos, Point3<f64>);
     access!(dir, Vector3<f64>);
     clone!(fov, f64);
-
-    /// Build a camera.
-    #[inline]
-    #[must_use]
-    pub fn build(&self) -> SimCamera {
-        SimCamera::new(
-            self.pos,
-            Unit::new_normalize(self.dir),
-            self.fov.to_radians(),
-        )
-    }
 }
