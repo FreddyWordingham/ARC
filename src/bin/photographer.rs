9ec83747da72d7a1e0894a3a9b2a48d95680e11e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-13 14:10:01 +0000

    Re-added whale universe setup.

--- src/bin/photographer.rs
+++ src/bin/photographer.rs
@@ -2,10 +2,10 @@
 
 use arc::{
     args,
-    file::Load,
-    // file::Save,
-    report,
+    file::{Camera as CameraForm, Grid as GridForm, Load, Save, Verse as VerseForm},
+    report, rows,
     util::{banner, exec, init},
+    world::Verse,
 };
 use attr::form;
 use colog;
@@ -16,7 +16,10 @@ use std::path::PathBuf;
 struct Parameters {
     num_threads: usize,
     num_phot: u64,
-    surf: PathBuf,
+    verse: VerseForm,
+    grid: GridForm,
+    cam: CameraForm,
+    res: (usize, usize),
 }
 
 pub fn main() {
@@ -31,7 +34,44 @@ pub fn main() {
 
     banner::section("Loading");
     info!("Loading parameters file...");
-    let _params = Parameters::load(&params_path);
+    let params = Parameters::load(&params_path);
+
+    info!("Loading universe files...");
+    let verse = params.verse.form(&in_dir);
+
+    banner::section("Building");
+    info!("Building grid...");
+    let grid = params.grid.form(params.num_threads, &verse);
+    let mat_maps = grid.mat_maps(verse.mats());
+    // let cam = params.cam.build();
+
+    banner::section("Overview");
+    overview(&verse);
+    info!("Material mapping breakdown:");
+    for (key, map) in mat_maps.map() {
+        let count = map.sum();
+        let fraction = count / map.len() as f64 * 100.0;
+        rows!(format!("{}", key), count, format!("{}%", fraction));
+    }
+    let inter_boundaries = grid.inter_boundaries();
+
+    banner::section("Saving");
+    for (key, map) in mat_maps.map() {
+        map.save(&out_dir.join(format!("mat_map_{}.nc", key)));
+    }
+    inter_boundaries.save(&out_dir.join("boundaries_interfaces.nc"));
+
+    banner::section("Simulation");
+    // let img = imager::run(
+    //     params.num_threads,
+    //     params.num_phot,
+    //     &LightKey::new("overhead"),
+    //     &cam,
+    //     params.res,
+    //     &verse,
+    //     &grid,
+    // );
+    // img.save(&out_dir.join("img.nc"));
 
     banner::section("Finished");
 }
@@ -44,4 +84,41 @@ fn initialisation() -> (PathBuf, PathBuf, PathBuf) {
     let params_path = &in_dir.join(params_name);
 
     (in_dir, out_dir, params_path.to_path_buf())
+}
+
+fn overview(verse: &Verse) {
+    info!("{} interfaces:", verse.inters().map().len());
+    for key in verse.inters().map().keys() {
+        info!("\t{}", key);
+    }
+
+    info!("{} reactions:", verse.reacts().map().len());
+    for key in verse.reacts().map().keys() {
+        info!("\t{}", key);
+    }
+
+    info!("{} lights:", verse.lights().map().len());
+    for key in verse.lights().map().keys() {
+        info!("\t{}", key);
+    }
+
+    info!("{} materials:", verse.mats().map().len());
+    for key in verse.mats().map().keys() {
+        info!("\t{}", key);
+    }
+
+    info!("{} states:", verse.states().map().len());
+    for key in verse.states().map().keys() {
+        info!("\t{}", key);
+    }
+
+    info!("{} species:", verse.specs().map().len());
+    for key in verse.specs().map().keys() {
+        info!("\t{}", key);
+    }
+
+    info!("{} surfaces:", verse.surfs().map().len());
+    for key in verse.surfs().map().keys() {
+        info!("\t{}", key);
+    }
 }
