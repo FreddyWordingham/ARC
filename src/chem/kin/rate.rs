26e9a9d4030941f108494dcb3ea1a1d39c2163d6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-17 23:24:51 +0000

    Added mutlivariate creation method to rate structure.

--- src/chem/kin/rate.rs
+++ src/chem/kin/rate.rs
@@ -1,7 +1,11 @@
 //! Rate implementation.
 
-use crate::ord::SpecKey;
+use crate::{
+    math::Multivariate,
+    ord::{SpecKey, SpecSet},
+};
 use attr::json;
+use ndarray::Array1;
 use std::fmt::{Display, Formatter, Result};
 
 /// Rates that accept a single scalar value, and return a single scalar value.
@@ -17,6 +21,34 @@ pub enum Rate {
     Third(f64, SpecKey, SpecKey, SpecKey),
     /// Polyadic. f(cs) = prod(k[n])
     Poly(f64, Vec<SpecKey>),
+}
+
+impl Rate {
+    /// Create a multivariate lambda function to apply the reaction to a concentration array.
+    pub fn create_lambda(&self, specs: SpecSet) -> Multivariate {
+        match self {
+            Self::Zeroth(k) => Multivariate::new_constant(*k),
+            Self::First(k, a) => Multivariate::new_scaled_first_order(*k, specs.index_of_key(a)),
+            Self::Second(k, a, b) => Multivariate::new_scaled_second_order(
+                *k,
+                specs.index_of_key(a),
+                specs.index_of_key(b),
+            ),
+            Self::Third(k, a, b, c) => Multivariate::new_scaled_third_order(
+                *k,
+                specs.index_of_key(a),
+                specs.index_of_key(b),
+                specs.index_of_key(c),
+            ),
+            Self::Poly(k, cs) => {
+                let mut ws = Array1::zeros(specs.map().len());
+                for c in cs {
+                    *ws.get_mut(specs.index_of_key(c)).expect("Invalid index.") = 1.0;
+                }
+                Multivariate::new_scaled_weight(*k, ws)
+            }
+        }
+    }
 }
 
 impl Display for Rate {
--- src/math/lambda/multivariate.rs
+++ src/math/lambda/multivariate.rs
@@ -8,8 +8,47 @@ use ndarray::Array1;
 pub enum Multivariate {
     /// Sum
     Sum,
+    /// Constant
+    Constant {
+        /// Constant value.
+        c: f64,
+    },
+    ScaledFirstOrder {
+        /// Scaling constant.
+        k: f64,
+        /// First order index.
+        a: usize,
+    },
+    ScaledSecondOrder {
+        /// Scaling constant.
+        k: f64,
+        /// First order index.
+        a: usize,
+        /// Second order index.
+        b: usize,
+    },
+    ScaledThirdOrder {
+        /// Scaling constant.
+        k: f64,
+        /// First order index.
+        a: usize,
+        /// Second order index.
+        b: usize,
+        /// Third order index.
+        c: usize,
+    },
     /// Weighting
-    Weight { ws: Array1<f64> },
+    Weight {
+        /// Weightings for each respective input value.
+        ws: Array1<f64>,
+    },
+    /// Scaled weighting
+    ScaledWeight {
+        /// Scaling constant.
+        k: f64,
+        /// Weightings for each respective input value.
+        ws: Array1<f64>,
+    },
 }
 
 impl Multivariate {
@@ -20,11 +59,46 @@ impl Multivariate {
         Self::Sum {}
     }
 
+    /// Construct a new constant instance.
+    #[inline]
+    #[must_use]
+    pub fn new_constant(c: f64) -> Self {
+        Self::Constant { c }
+    }
+
+    /// Construct a new scaled first order instance.
+    #[inline]
+    #[must_use]
+    pub fn new_scaled_first_order(k: f64, a: usize) -> Self {
+        Self::ScaledFirstOrder { k, a }
+    }
+
+    /// Construct a new scaled second order instance.
+    #[inline]
+    #[must_use]
+    pub fn new_scaled_second_order(k: f64, a: usize, b: usize) -> Self {
+        Self::ScaledSecondOrder { k, a, b }
+    }
+
+    /// Construct a new scaled third order instance.
+    #[inline]
+    #[must_use]
+    pub fn new_scaled_third_order(k: f64, a: usize, b: usize, c: usize) -> Self {
+        Self::ScaledThirdOrder { k, a, b, c }
+    }
+
     /// Construct a new weighted instance.
     #[inline]
     #[must_use]
     pub fn new_weight(ws: Array1<f64>) -> Self {
         Self::Weight { ws }
+    }
+
+    /// Construct a new scaled weighted instance.
+    #[inline]
+    #[must_use]
+    pub fn new_scaled_weight(k: f64, ws: Array1<f64>) -> Self {
+        Self::ScaledWeight { k, ws }
     }
 
     /// Determine the corresponding output value for the given input.
@@ -33,7 +107,27 @@ impl Multivariate {
     pub fn y(&self, xs: &Array1<f64>) -> f64 {
         match self {
             Self::Sum {} => xs.sum(),
-            Self::Weight { ws } => { xs.iter().zip(ws).map(|(x, w)| x * w) }.sum(),
+            Self::Constant { c } => *c,
+            Self::ScaledFirstOrder { k, a } => *xs.get(*a).expect("Invalid index.") * k,
+            Self::ScaledSecondOrder { k, a, b } => {
+                *xs.get(*a).expect("Invalid index.") * *xs.get(*b).expect("Invalid index.") * k
+            }
+            Self::ScaledThirdOrder { k, a, b, c } => {
+                *xs.get(*a).expect("Invalid index.")
+                    * *xs.get(*b).expect("Invalid index.")
+                    * *xs.get(*c).expect("Invalid index.")
+                    * k
+            }
+            Self::Weight { ws } => {
+                assert!(xs.len() == ws.len());
+
+                xs.iter().zip(ws).map(|(x, w)| x * w).sum()
+            }
+            Self::ScaledWeight { k, ws } => {
+                assert!(xs.len() == ws.len());
+
+                xs.iter().zip(ws).map(|(x, w)| x * w).sum::<f64>() * k
+            }
         }
     }
 }
