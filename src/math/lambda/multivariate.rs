e4e40de86160dc97ac5170245f242ce5d8cf0b03
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-17 22:55:41 +0000

    Added weight multivariate variant.

--- src/math/lambda/multivariate.rs
+++ src/math/lambda/multivariate.rs
@@ -8,6 +8,8 @@ use ndarray::Array1;
 pub enum Multivariate {
     /// Sum
     Sum,
+    /// Weighting
+    Weight { ws: Array1<f64> },
 }
 
 impl Multivariate {
@@ -18,12 +20,20 @@ impl Multivariate {
         Self::Sum {}
     }
 
+    /// Construct a new weighted instance.
+    #[inline]
+    #[must_use]
+    pub fn new_weight(ws: Array1<f64>) -> Self {
+        Self::Weight { ws }
+    }
+
     /// Determine the corresponding output value for the given input.
     #[inline]
     #[must_use]
     pub fn y(&self, xs: &Array1<f64>) -> f64 {
         match self {
             Self::Sum {} => xs.sum(),
+            Self::Weight { ws } => { xs.iter().zip(ws).map(|(x, w)| x * w) }.sum(),
         }
     }
 }
