73289e27b8d440364e24a856d6ce49547a4c777d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-03-30 15:55:40 +0100

    Added camera form.

---
+++ src/file/json/camera.rs
@@ -0,0 +1,32 @@
+//! Camera implementation.
+
+use crate::{access, clone, sim::render::Camera as SimCam};
+use attr::json;
+use nalgebra::Point3;
+
+/// Camera building structure.
+#[json]
+pub struct Camera {
+    /// Viewing position.
+    pos: Point3<f64>,
+    /// Target viewing point.
+    tar: Point3<f64>,
+    /// Field of view [deg].
+    fov: f64,
+    /// Image resolution.
+    res: (usize, usize),
+}
+
+impl Camera {
+    access!(pos, Point3<f64>);
+    access!(tar, Point3<f64>);
+    clone!(fov, f64);
+    clone!(res, (usize, usize));
+
+    /// Build a simulation camera.
+    #[inline]
+    #[must_use]
+    pub fn build(&self) -> SimCam {
+        SimCam::new(self.pos, self.tar, self.fov.to_radians(), self.res)
+    }
+}
--- src/file/json/mod.rs
+++ src/file/json/mod.rs
@@ -1,7 +1,8 @@
 //! Json sub-module.
 
+pub mod camera;
 pub mod probability;
 pub mod surface;
 pub mod transform;
 
-pub use self::{probability::*, surface::*, transform::*};
+pub use self::{camera::*, probability::*, surface::*, transform::*};
