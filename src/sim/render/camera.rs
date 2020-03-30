0e614871db9bc4cec0e6a3194898f2de69d4abac
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-03-30 15:20:55 +0100

    Added camera fields.

--- src/sim/render/camera.rs
+++ src/sim/render/camera.rs
@@ -12,19 +12,19 @@ pub struct Camera {
     up: Unit<Vector3<f64>>,
     /// Right axis.
     right: Unit<Vector3<f64>>,
-    // /// Field of view.
-    // fov: (f64, f64),
-    // /// Image resolution.
-    // res: (usize, usize),
-    // /// Scanning deltas.
-    // delta: (f64, f64),
+    /// Field of view.
+    fov: (f64, f64),
+    /// Image resolution.
+    res: (usize, usize),
+    /// Scanning deltas.
+    delta: (f64, f64),
 }
 
 impl Camera {
     access!(forward, Ray);
     access!(up, Vector3<f64>);
     access!(right, Vector3<f64>);
-    // clone!(fov, (f64, f64));
-    // clone!(res, (usize, usize));
-    // clone!(delta, (f64, f64));
+    clone!(fov, (f64, f64));
+    clone!(res, (usize, usize));
+    clone!(delta, (f64, f64));
 }
