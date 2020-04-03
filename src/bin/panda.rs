dda082a9f76442b13ee9cccfe74e56b391154b5b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-03 16:07:12 +0100

    Added panda rendering binary.

---
+++ src/bin/panda.rs
@@ -0,0 +1,34 @@
+//! Panda rendering engine!
+
+use arc::{
+    args,
+    file::Load,
+    report,
+    util::{banner, exec, init},
+};
+use attr::form;
+use log::info;
+
+#[form]
+struct Parameters {}
+
+fn main() {
+    colog::init();
+    banner::title(&exec::name());
+    banner::section("initialisation");
+    args!(_bin_path: String;
+        params_name: String);
+
+    let (in_dir, out_dir) = init::io_dirs(None, None);
+    let params_path = &in_dir.join(params_name);
+
+    report!(in_dir.display(), "input directory");
+    report!(out_dir.display(), "output directory");
+    report!(params_path.display(), "parameters path");
+
+    banner::section("Loading");
+    info!("Loading parameters file");
+    let params = Parameters::load(&params_path);
+
+    arc::report!(params);
+}
