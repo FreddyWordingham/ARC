dcf9c51933dbf873d6e1129e06297609c63d5a20
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-03 20:02:27 +0100

    Added cell finding methods.

--- src/sim/panda/cell.rs
+++ src/sim/panda/cell.rs
@@ -176,7 +176,40 @@ impl<'a> Cell<'a> {
         Self::Branch { boundary, children }
     }
 
+    /// Determine the terminal cell containing the given position.
+    #[inline]
+    #[must_use]
+    pub fn find_terminal_cell(&self, pos: &Point3<f64>) -> Option<&Self> {
+        if !self.boundary().contains(pos) {
+            return None;
+        }
+
+        match self {
+            Self::Leaf { .. } | Self::Empty { .. } => Some(&self),
+            Self::Root { boundary, children } | Self::Branch { boundary, children } => {
+                let mut index = 0;
+                let c = boundary.centre();
+
+                if pos.x >= c.x {
+                    index += 1;
+                }
+                if pos.y >= c.y {
+                    index += 2;
+                }
+                if pos.z >= c.z {
+                    index += 4;
+                }
+                children
+                    .get(index)
+                    .expect("Invalid index")
+                    .find_terminal_cell(pos)
+            }
+        }
+    }
+
     /// Reference the cell's boundary.
+    #[inline]
+    #[must_use]
     pub fn boundary(&self) -> &Aabb {
         match self {
             Self::Root { boundary, .. }
