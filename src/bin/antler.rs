6067a39531e81783242cb251e8746e478e7001d8
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-05 20:02:52 +0100

    Added initialisation.

--- src/bin/antler.rs
+++ src/bin/antler.rs
@@ -1,5 +1,35 @@
 //! Antler rendering engine.
 
+use arc::{
+    args, report,
+    util::{banner, exec, init},
+};
+use attr::form;
+use log::info;
+use std::path::PathBuf;
+
+#[form]
+struct Parameters {}
+
 fn main() {
-    println!("Hello, world!");
+    colog::init();
+    banner::title(&exec::name());
+
+    banner::section("Initialisation");
+    let (_in_dir, _out_dir, _param_path) = start();
+}
+
+fn start() -> (PathBuf, PathBuf, PathBuf) {
+    args!(_bin_path: String;
+        params_name: String
+    );
+
+    let (in_dir, out_dir) = init::io_dirs(None, None);
+    let params_path = in_dir.join(params_name);
+
+    report!(in_dir.display(), "input directory");
+    report!(out_dir.display(), "output directory");
+    report!(params_path.display(), "parameters path");
+
+    (in_dir, out_dir, params_path)
 }
