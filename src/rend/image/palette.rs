14ef440ee0805f660ecf93d8f496c9fa10f6c449
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-07 00:35:40 +0100

    Added palette loading.

--- src/rend/image/palette.rs
+++ src/rend/image/palette.rs
@@ -1,13 +1,22 @@
 //! Palette implementation.
 
+use crate::{access, rend::Group};
+use palette::{Gradient, LinSrgba};
+use std::collections::HashMap;
+
 /// Image colouring structure.
-pub struct Palette {}
+pub struct Palette {
+    /// Group colours.
+    grads: HashMap<Group, Gradient<LinSrgba>>,
+}
 
 impl Palette {
+    access!(grads, HashMap<Group, Gradient<LinSrgba>>);
+
     /// Construct a new instance.
     #[inline]
     #[must_use]
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(grads: HashMap<Group, Gradient<LinSrgba>>) -> Self {
+        Self { grads }
     }
 }
--- src/rend/settings/palette.rs
+++ src/rend/settings/palette.rs
@@ -5,21 +5,47 @@ use crate::{
     rend::{image::Palette as ImagePalette, Group},
 };
 use attr::json;
+use palette::{Gradient, LinSrgba};
+use std::collections::HashMap;
 
 /// Palette settings.
 #[json]
 pub struct Palette {
     /// Group colours.
-    cols: Vec<(Group, Vec<[f64; 4]>)>,
+    grads: Vec<(Group, Vec<[f64; 4]>)>,
 }
 
 impl Palette {
-    access!(cols, Vec<(Group, Vec<[f64; 4]>)>);
+    access!(grads, Vec<(Group, Vec<[f64; 4]>)>);
 
     /// Build a complete instance.
     #[inline]
     #[must_use]
     pub fn build(&self) -> ImagePalette {
-        ImagePalette::new()
+        let mut list = HashMap::with_capacity(self.grads.len());
+
+        for (group, cols) in &self.grads {
+            if list.contains_key(group) {
+                panic!("Duplicate gradient for group: {}", group);
+            }
+
+            list.insert(
+                *group,
+                Gradient::new(
+                    cols.iter()
+                        .map(|col| {
+                            LinSrgba::new(
+                                *col.get(0).expect("Missing alpha channel entry.") as f32,
+                                *col.get(1).expect("Missing red channel entry.") as f32,
+                                *col.get(2).expect("Missing blue channel entry.") as f32,
+                                *col.get(3).expect("Missing green channel entry.") as f32,
+                            )
+                        })
+                        .collect::<Vec<_>>(),
+                ),
+            );
+        }
+
+        ImagePalette::new(list)
     }
 }
