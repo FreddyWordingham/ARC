4b2b62eb8b6ac07aaac111c73919d88cd96866f9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-06 13:21:22 +0100

    Added cols macro.

--- src/fmt/cols.rs
+++ src/fmt/cols.rs
@@ -3,24 +3,14 @@
 /// Print to equally spaced columns.
 #[macro_export]
 macro_rules! cols {
-    ($num_cols: expr, $total_width: expr, $e0: expr) => {
-        format!("{:<width$}", $e0, width = $total_width / $num_cols)
-    };
-    ($num_cols: expr, $total_width: expr, $e0: expr, $e1: expr) => {
-        format!(
-            "{:<width$}{:<width$}",
-            $e0,
-            $e1,
-            width = $total_width / $num_cols
-        )
-    };
-    ($num_cols: expr, $total_width: expr, $e0: expr, $e1: expr, $e2: expr) => {
-        format!(
-            "{:<width$}{:<width$}{:<width$}",
-            $e0,
-            $e1,
-            $e2,
-            width = $total_width / $num_cols
-        )
+    ($num_cols: expr, $total_width: expr, $($val: expr),*) => {
+        {
+            use std::fmt::Write;
+
+            let mut string = String::new();
+            $(write!(string, "{:<width$}", $val, width = $total_width / $num_cols).expect("Failed to write to string buffer.");)*
+
+            string
+        }
     };
 }
