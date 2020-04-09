ffa2c54aef708607e49f7fe04fe5f668fd86b5e3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-09 15:01:43 +0100

    Added reflections.

--- src/sim/render/painter/test.rs
+++ src/sim/render/painter/test.rs
@@ -3,6 +3,7 @@
 use crate::{
     geom::Ray,
     img::Shader,
+    phys::laws::reflect_dir,
     sim::render::{lighting, shadowing, Grid, Scheme},
 };
 use nalgebra::{Point3, Unit};
@@ -25,23 +26,26 @@ pub fn paint(
 ) -> LinSrgba {
     let mut col = LinSrgba::default();
     while let Some(hit) = grid.observe(ray.clone(), shader.bump_dist()) {
-        ray.travel(hit.dist() + shader.bump_dist());
+        ray.travel(hit.dist());
+
+        let light_dir = Unit::new_normalize(shader.sun_pos() - ray.pos());
+        let view_dir = Unit::new_normalize(cam_pos - ray.pos());
+
+        let light = lighting::ambient(shader)
+            + lighting::diffuse(shader, hit.side().norm(), &light_dir)
+            + lighting::specular(shader, hit.side().norm(), &light_dir, &view_dir);
+        let shadow = shadowing::direct(grid, shader, ray.clone(), hit.side().norm());
+
+        let x = light * shadow;
 
         match hit.group() {
-            90..=99 => {
-                break;
+            23..=25 => {
+                *ray.dir_mut() = reflect_dir(ray.dir(), hit.side().norm());
+                ray.travel(shader.bump_dist());
+
+                col += scheme.get(hit.group()).get(x as f32) * 0.1;
             }
             _ => {
-                let light_dir = Unit::new_normalize(shader.sun_pos() - ray.pos());
-                let view_dir = Unit::new_normalize(cam_pos - ray.pos());
-
-                let light = lighting::ambient(shader)
-                    + lighting::diffuse(shader, hit.side().norm(), &light_dir)
-                    + lighting::specular(shader, hit.side().norm(), &light_dir, &view_dir);
-                let shadow = shadowing::direct(grid, shader, ray, hit.side().norm());
-
-                let x = light * shadow;
-
                 col += scheme.get(hit.group()).get(x as f32);
                 break;
             }
