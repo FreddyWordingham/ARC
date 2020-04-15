60473503b4aa8ef46884710e22f0fb09a48f2b10
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-04-15 20:30:55 +0100

    Added refractive rocks.

--- src/sim/render/painter/arctic.rs
+++ src/sim/render/painter/arctic.rs
@@ -3,7 +3,7 @@
 use crate::{
     geom::Ray,
     img::Shader,
-    phys::laws::reflect_dir,
+    phys::{laws::reflect_dir, Crossing},
     sim::render::{Grid, Scheme},
 };
 use nalgebra::{Point3, Unit, Vector3};
@@ -16,8 +16,8 @@ const MIN_WEIGHT: f64 = 0.01;
 /// Mirror colouring fraction.
 const MIRROR_COLOURING: f32 = 0.15;
 
-// /// Refraction colouring fraction.
-// const REFRACTION_COLOURING: f32 = 0.01;
+/// Refraction colouring fraction.
+const REFRACTION_COLOURING: f32 = 0.20;
 
 /// Puddle reflection shimmer factor.
 const PUDDLE_SHIMMER: f64 = 12.0;
@@ -33,7 +33,7 @@ pub fn paint(
     shader: &Shader,
     scheme: &Scheme,
     mut ray: Ray,
-    _rng: &mut ThreadRng,
+    rng: &mut ThreadRng,
     weight: f64,
 ) -> LinSrgba {
     debug_assert!(shader.bump_dist() > 0.0);
@@ -49,7 +49,7 @@ pub fn paint(
         ray.travel(hit.dist());
 
         let light = light(cam_pos, shader, &ray, hit.side().norm());
-        let shadow = 1.0;
+        let shadow = shadow(grid, shader, &ray, hit.side().norm());
         let illumination = light * shadow;
 
         match hit.group() {
@@ -64,6 +64,47 @@ pub fn paint(
                 let rot = nalgebra::Rotation3::from_axis_angle(&nalgebra::Vector3::y_axis(), theta);
                 *ray.dir_mut() = nalgebra::Unit::new_normalize(rot * ray.dir().as_ref());
                 ray.travel(shader.bump_dist());
+            }
+            11 => {
+                // Rocks 1
+                col += scheme.get(hit.group()).get(illumination as f32) * REFRACTION_COLOURING;
+
+                let crossing = Crossing::new(ray.dir(), hit.side().norm(), 1.0, 1.1);
+                if let Some(trans_dir) = crossing.trans_dir() {
+                    let ref_prob = crossing.ref_prob();
+                    let trans_prob = 1.0 - ref_prob;
+
+                    let mut ref_ray =
+                        Ray::new(ray.pos().clone(), reflect_dir(ray.dir(), hit.side().norm()));
+                    ref_ray.travel(shader.bump_dist());
+                    let ref_col = paint(
+                        cam_pos,
+                        grid,
+                        shader,
+                        scheme,
+                        ref_ray,
+                        rng,
+                        weight * ref_prob,
+                    );
+
+                    let mut trans_ray = ray;
+                    *trans_ray.dir_mut() = *trans_dir;
+                    trans_ray.travel(shader.bump_dist());
+                    let trans_col = paint(
+                        cam_pos,
+                        grid,
+                        shader,
+                        scheme,
+                        trans_ray,
+                        rng,
+                        weight * trans_prob,
+                    );
+
+                    return col + (ref_col * ref_prob as f32) + (trans_col * trans_prob as f32);
+                } else {
+                    *ray.dir_mut() = reflect_dir(ray.dir(), hit.side().norm());
+                    ray.travel(shader.bump_dist());
+                }
             }
             _ => {
                 col += scheme.get(hit.group()).get(illumination as f32);
@@ -95,4 +136,48 @@ fn light(cam_pos: &Point3<f64>, shader: &Shader, ray: &Ray, norm: &Unit<Vector3<
     specular *= shader.light_weights().specular();
 
     ambient + diffuse + specular
+}
+
+/// Calculate the shadowing multiplier. 0.0 = Full shadow. 1.0 = No shadow.
+#[inline]
+#[must_use]
+fn shadow(grid: &Grid, shader: &Shader, ray: &Ray, norm: &Unit<Vector3<f64>>) -> f64 {
+    let mut light_ray = Ray::new(*ray.pos(), *norm);
+    light_ray.travel(shader.bump_dist());
+    let light_dir = Unit::new_normalize(shader.sun_pos() - light_ray.pos());
+    *light_ray.dir_mut() = light_dir;
+
+    let mut direct = visibility(grid, shader, light_ray);
+
+    direct *= shader.shadow_weights().direct();
+
+    direct
+}
+
+/// Calculate the visibility of a given ray.
+#[allow(clippy::never_loop)]
+#[allow(clippy::single_match_else)]
+#[inline]
+#[must_use]
+fn visibility(grid: &Grid, shader: &Shader, mut ray: Ray) -> f64 {
+    let mut vis = 1.0;
+    while let Some(hit) = grid.observe(ray.clone(), shader.bump_dist()) {
+        if vis <= 0.0 {
+            break;
+        }
+
+        let mut dist = hit.dist();
+
+        match hit.group() {
+            _ => {
+                // Opaque
+                vis = 0.1;
+                break;
+            }
+        }
+
+        ray.travel(dist);
+    }
+
+    vis
 }
